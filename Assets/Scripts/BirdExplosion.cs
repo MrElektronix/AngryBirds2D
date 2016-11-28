1ab527aeadf7a0d4a1701d48128f53da12c54b53
Justin van Gemen <20277@ma-web.nl>
2016-11-28 14:14:23 +0100

    Creation of script

--- Assets/Scripts/BirdExplosion.cs
+++ Assets/Scripts/BirdExplosion.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class random : MonoBehaviour {
+public class BirdExplosion : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
