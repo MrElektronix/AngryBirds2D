67c1bff494ec453252796cb4fa70fcd4c4517dc2
Justin van Gemen <20277@ma-web.nl>
2016-12-04 20:51:25 +0100

    Bird Explosion

--- Assets/Scripts/BirdExplosion.cs
+++ Assets/Scripts/BirdExplosion.cs
@@ -3,29 +3,16 @@ using System.Collections;
 
 public class BirdExplosion : MonoBehaviour {
 
-    public Vector2 force = new Vector2 (10, 10);
-    private Rigidbody2D rb;
-    private Vector2 explosionPos;
+    public GameObject Explosion;
 
-    void Start ()
+    void Update()
     {
-        rb = GetComponent<Rigidbody2D>();
-        StartCoroutine(ExplosionWait());
-    }
-	
-	
-	void Update ()
-    {
-        Vector2 explosionPos = transform.position;
-    }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Instantiate(Explosion, transform.position, Quaternion.identity);
+            gameObject.SetActive(false);
+        }
 
-    private IEnumerator ExplosionWait()
-    {
-        yield return new WaitForSeconds(6);
-        Debug.Log("Explode");
-        //rb.AddForce(explosionPos, ForceMode2D.Impulse);
-        rb.AddForceAtPosition(force, explosionPos, ForceMode2D.Force);
-        
     }
 
 }
