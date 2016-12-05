1d4d1ed133cb276ce56c948cc52aa60deda4f40d
Justin van Gemen <20277@ma-web.nl>
2016-12-05 14:16:15 +0100

    Explosie Animatie Fix

---
+++ Assets/Scripts/RemoveObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemoveObject : MonoBehaviour {
+
+	void Start () {
+        
+        Destroy(gameObject, 0.4f);
+        
+    }
+
+}
