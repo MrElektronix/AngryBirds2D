e7f96e801f10921f3fe59ef59e68cf19a60aeb24
MrElektronix <milovanpelt@gmail.com>
2016-12-12 19:05:09 +0100

    update

--- Assets/Scripts/BlockDamage.cs
+++ Assets/Scripts/BlockDamage.cs
@@ -1,16 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BlockDamage : MonoBehaviour {
+public class BlockDamage : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask layerToIgnore;
 
+    [SerializeField]
+    private float radius;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start()
+    {
+        DamageBlocks();
+    }
+
+    void DamageBlocks()
+    {
+        Collider2D[] blocks = Physics2D.OverlapCircleAll(transform.position, radius, layerToIgnore);
+        for (int i = 0; i < blocks.Length; i++)
+        {
+            Debug.Log(blocks[i].gameObject);
+            float dmg = radius - Vector3.Distance(transform.position, blocks[i].transform.position);
+            dmg = Mathf.Clamp(dmg, 0, Mathf.Infinity);
+            blocks[i].gameObject.SendMessage("AddDamage", dmg);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
 }
---
+++ Assets/Scripts/BlockHealth.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockHealth : MonoBehaviour {
+    [SerializeField]
+    private float health;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void AddDamage(float damage)
+    {
+        health -= damage;
+        Debug.Log(damage);
+    }
+}
